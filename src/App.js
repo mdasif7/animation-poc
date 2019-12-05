bb86b966a5497bd68c0ebd6cfd3ca4366561749d
masif7 <shadmanasif10@gmail.com>
2019-12-05 20:47:11 +0530

    CSS animation POC

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Animate the boxes given below as shown in
+        <a href="https://player.vimeo.com/video/356567201">this video!</a>
+        <br />
+        <i>Note: You can use JS/CSS to solve the problem.</i>
+      </div>
+      <br />
+
+      <div className="container">
+        <div className="stone">R</div>
+        <div className="stone">E</div>
+        <div className="stone">D</div>
+        <div className="stone">B</div>
+        <div className="stone">O</div>
+        <div className="stone">O</div>
+        <div className="stone">K</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
